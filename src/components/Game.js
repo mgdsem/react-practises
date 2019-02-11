e16384d786e35be45b777d8667f1067bbfb4ff1c
Magdalena Semik <mgd.kaczmarek@gmail.com>
2019-02-11 19:08:03 +0100

    make game

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,10 +12,11 @@ class Game extends Component {
         this.onResetGame = this.onResetGame.bind(this);
 
         this.state = {
-            round: 0,
-            numberToGuess: Math.floor((Math.random() * 10) + 1),
-            numberForHint: 0,
-            currentInputValue: ''
+            roundName: 'round:',
+            round: 1,
+            numberToGuess: Math.floor((Math.random() * 100) + 1),
+            currentInputValue: '',
+            gameStatusMessage: ''
         }
     };
 
@@ -25,16 +26,32 @@ class Game extends Component {
     }
 
     onGo() {
-        console.log(this.state.numberToGuess);
         this.setState(prevState => ({
             round: prevState.round + 1
         }))
-    }
+        console.log(this.state.numberToGuess);
+        console.log(this.state.currentInputValue);
+
+        this.setState({ currentInputValue: '' });
+
+        if (Number(this.state.currentInputValue) > 100 || Number(this.state.currentInputValue) < 0) {
+            this.setState({ gameStatusMessage: 'You were supposed to put a number between 0 and 100' });
+        } else if (this.state.numberToGuess > Number(this.state.currentInputValue)) {
+            this.setState({ gameStatusMessage: 'Try something higher' });
+        } else if (this.state.numberToGuess < Number(this.state.currentInputValue)) {
+            this.setState({ gameStatusMessage: "try something lower" });
+        } else {
+            this.setState({ gameStatusMessage: `You won in ${this.state.round} rounds!` })
+            this.setState({ round: '', roundName: '' })
+        };
+    };
 
     onResetGame() {
         this.setState({ currentInputValue: '' });
-        this.setState({ numberToGuess: Math.floor((Math.random() * 10) + 1) })
-
+        this.setState({ numberToGuess: Math.floor((Math.random() * 100) + 1) });
+        this.setState({ round: 1 });
+        this.setState({ roundName: 'round:' })
+        this.setState({ gameStatusMessage: '' });
     }
 
     render() {
@@ -50,10 +67,10 @@ class Game extends Component {
                 />
                 <Button onClick={this.onGo}>Go!</Button>
                 <Button onClick={this.onResetGame}>Reset</Button>
-                <p>{this.state.round}</p>
+                <p>{this.state.roundName}{this.state.round}</p>
+                <p>{this.state.gameStatusMessage}</p>
             </div>
         )
-
     }
 };
 
